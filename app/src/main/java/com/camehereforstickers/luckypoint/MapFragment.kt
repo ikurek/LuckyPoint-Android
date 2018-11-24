469d09fd7baa1db4e17d4afda382a70b040f0f8a
KevinHomeAlone <kevinpieprz@gmail.com>
2018-11-24 16:24:01 +0100

    Uncommitted changes before Merge at 24.11.2018 16:20

--- app/src/main/java/com/camehereforstickers/luckypoint/MapFragment.kt
+++ app/src/main/java/com/camehereforstickers/luckypoint/MapFragment.kt
@@ -6,15 +6,35 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.android.synthetic.main.fragment_map.view.*
 
-class MapFragment : Fragment() {
+
+class MapFragment : Fragment(), OnMapReadyCallback{
+
+    private lateinit var mMap : GoogleMap
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_map, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        view.map_view.getMapAsync(this)
+        return view
     }
 
 
